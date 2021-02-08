64c108204a9ce8b0748dd5d9e6437d6dba9a5dda
shushuTona <nagase.syunnsuke@gmail.com>
2021-02-08 20:33:50 +0900

    atoms.table.vue追加

---
+++ src/components/atoms/atoms.table.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="m-table">
+        <table class="table__inner">
+            <colgroup>
+                <col class="table__col">
+                <col class="table__col">
+            </colgroup>
+            <tbody>
+                <tr
+                    v-for="tr of tableList"
+                    :key="tr.id"
+                >
+                    <th>{{ tr.th.content }}</th>
+                    <td
+                        v-for="td of tr.tdList"
+                        :key="td.id"
+                    >
+                        {{ td.content }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Table',
+    props: {
+        tableList: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.m-table {
+    margin: 24px 0;
+
+    .table {
+        &__inner {
+            margin: 0;
+            width: 100%;
+            border-collapse: collapse;
+
+            tr {
+                & + tr {
+                    border-top: 1px solid $BORDER_COLOR;
+                }
+            }
+
+            th,
+            td {
+                padding: 16px 0;
+            }
+
+            th {
+                font-weight: normal;
+                text-align: left;
+                padding-right: 16px;
+            }
+
+            td {
+                padding-left: 16px;
+            }
+        }
+
+        &__col {
+            &:nth-of-type(1) {
+                width: 30%;
+            }
+
+            &:nth-of-type(2) {
+                width: 70%;
+            }
+        }
+    }
+}
+</style>
--- src/views/views.about.vue
+++ src/views/views.about.vue
@@ -3,15 +3,13 @@
         <FirstView>
             <Heading1 :headingText="'About'" />
 
-            <p>僕に関しての軽い説明など</p>
+            <p>僕に関しての軽い説明</p>
         </FirstView>
 
         <Heading2 :headingText="'自己紹介'" />
         <p>フロントエンドのエンジニアとして都内の事業会社で働いています。最近転職しました。</p>
 
-        <p>2018年4月～2020年12月：新宿のweb制作会社でフロントエンドエンジニアとして勤務</p>
-
-        <p>2021年1月～：現在新宿の事業会社でエンジニアとして勤務中</p>
+        <Table :tableList="tableList" />
 
         <Heading2 :headingText="'Twitter'" />
         <p>Twitterのタイムラインを表示する。</p>
@@ -25,13 +23,45 @@
 import Heading1 from '@/components/atoms/atoms.heading1.vue';
 import Heading2 from '@/components/atoms/atoms.heading2.vue';
 import FirstView from '@/components/atoms/atoms.firstView.vue';
+import Table from '@/components/atoms/atoms.table.vue';
 
 export default {
     name: 'About',
     components: {
         Heading1,
         Heading2,
-        FirstView
+        FirstView,
+        Table
+    },
+    data() {
+        return {
+            tableList: [
+                {
+                    id: 0,
+                    th: {
+                        content: '2018年4月～2020年12月'
+                    },
+                    tdList: [
+                        {
+                            id: 0,
+                            content: '新宿のweb制作会社でフロントエンドエンジニアとして勤務'
+                        }
+                    ]
+                },
+                {
+                    id: 1,
+                    th: {
+                        content: '2021年1月～'
+                    },
+                    tdList: [
+                        {
+                            id: 0,
+                            content: '新宿の事業会社でエンジニアとして勤務'
+                        }
+                    ]
+                }
+            ]
+        }
     }
 }
 </script>
