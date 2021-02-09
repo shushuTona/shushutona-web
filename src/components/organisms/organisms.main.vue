b32d98c6d35b9a0de79813352d18cda3b2ab0247
shushuTona <nagase.syunnsuke@gmail.com>
2021-02-09 20:47:54 +0900

    ルーティング時のトランジション表示追加

--- src/components/organisms/organisms.main.vue
+++ src/components/organisms/organisms.main.vue
@@ -1,21 +1,47 @@
 <template>
     <main class="l-main">
-        <router-view/>
-        <canvas class="main__canvas" ref="canvas"></canvas>
+        <transition
+            name="fade"
+            mode="out-in"
+        >
+            <router-view/>
+        </transition>
+
+        <canvas
+            class="main__canvas"
+            ref="canvas"
+        ></canvas>
     </main>
 </template>
 
 <script>
+import * as THREE from "three";
+
 export default {
-    name: 'Main'
+    name: 'Main',
+    mounted() {
+        console.log(THREE);
+    }
 }
 </script>
 
 <style scoped lang="scss">
+.fade {
+    &-enter-active,
+    &-leave-active {
+        transition: opacity .5s;
+    }
+
+    &-enter,
+    &-leave-to {
+        opacity: 0;
+    }
+}
+
 .l-main {
     & > [class^="views-"] {
         margin: 0 auto;
-        padding: 0 24px;
+        padding: 0 24px 64px;
         width: 100%;
         max-width: $CONTENT_WIDTH;
         position: relative;
