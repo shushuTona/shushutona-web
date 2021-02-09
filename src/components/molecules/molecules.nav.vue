3075b59562a54f15b7eee8b7d0a17a3d3cb8d529
shushuTona <nagase.syunnsuke@gmail.com>
2021-02-09 21:33:44 +0900

    グロナビhover＆カレント表示追加

--- src/components/molecules/molecules.nav.vue
+++ src/components/molecules/molecules.nav.vue
@@ -1,10 +1,10 @@
 <template>
     <nav class="header__nav">
         <ul class="header__navList">
-            <li class="header__navListItem"><router-link to="/">Home</router-link></li>
-            <li class="header__navListItem"><router-link to="/about">About</router-link></li>
-            <li class="header__navListItem"><router-link to="/product">Product</router-link></li>
-            <li class="header__navListItem"><router-link to="/skill">Skill</router-link></li>
+            <li class="header__navListItem"><router-link class="header__navListLink" to="/">Home</router-link></li>
+            <li class="header__navListItem"><router-link class="header__navListLink" to="/about">About</router-link></li>
+            <li class="header__navListItem"><router-link class="header__navListLink" to="/product">Product</router-link></li>
+            <li class="header__navListItem"><router-link class="header__navListLink" to="/skill">Skill</router-link></li>
         </ul>
     </nav>
 </template>
@@ -17,10 +17,6 @@ export default {
 
 <style scoped lang="scss">
 .header {
-    &__nav {
-
-    }
-
     &__navList {
         display: flex;
         justify-content: center;
@@ -30,16 +26,58 @@ export default {
         & + .header__navListItem {
             margin-left: 40px;
         }
+    }
 
-        a {
-            display: block;
-            font-size: 1.6em;
-            text-decoration: none;
-            color: $FONT_COLOR;
+    &__navListLink {
+        padding: 0 0 8px;
+        display: block;
+        font-size: 1.6em;
+        text-decoration: none;
+        color: $FONT_COLOR;
+        overflow: hidden;
+        position: relative;
 
-            @include breakPC() {
-                font-size: 2.4rem;
+        @include breakPC() {
+            font-size: 2.4rem;
+        }
+
+        // カレント以外のリンクhover表示
+        &:not([aria-current="page"]) {
+            &::before,
+            &::after {
+                content: "";
+                height: 4px;
+                transform: translateX(-100px);
+                transition: transform $TRANSITION;
+                transition-delay: 0;
+                position: absolute;
+                left: 0;
             }
+
+            &::before {
+                width: 100%;
+                background-color: $MAIN_COLOR;
+                bottom: 4px;
+            }
+
+            &::after {
+                width: 80%;
+                background-color: $SUB_COLOR;
+                bottom: 0;
+                transition-delay: .1s;
+            }
+
+            @include hover() {
+                &::before,
+                &::after {
+                    transform: translateX(0);
+                }
+            }
+        }
+
+        &[aria-current="page"] {
+            color: $MAIN_COLOR;
+            cursor: default;
         }
     }
 }
