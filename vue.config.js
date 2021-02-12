a5e0b96a5b55cb90fc035b89ee488b97cbc3977d
shushuTona <nagase.syunnsuke@gmail.com>
2021-02-13 00:07:45 +0900

    prerendering追加

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,28 @@
+const path = require( 'path' );
+const PrerenderSPAPlugin = require( 'prerender-spa-plugin' );
+
 module.exports = {
     css: {
         loaderOptions: {
             scss: {
                 additionalData: `@import "./src/assets/sass/prepends.scss";`
+            }
+        }
+    },
+    configureWebpack: (config) => {
+        if ( process.env.NODE_ENV === 'production' ) {
+            return {
+                plugins: [
+                    new PrerenderSPAPlugin( {
+                        staticDir: path.join( __dirname, 'dist' ),
+                        routes: [
+                            '/',
+                            '/about',
+                            '/product',
+                            '/skill'
+                        ]
+                    } )
+                ]
             }
         }
     }
