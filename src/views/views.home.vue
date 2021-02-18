df1f1ae89d92ca622df91e83e6bd4431031fa74b
shushuTona <nagase.syunnsuke@gmail.com>
2021-02-18 22:46:19 +0900

    homeにFirstView追加＆不要画像削除

--- src/views/views.home.vue
+++ src/views/views.home.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="views-home">
-        <Heading1 :headingText="'Shushutona-web'" />
+        <FirstView>
+            <Heading1 :headingText="'Shushutona-web'" />
+        </FirstView>
 
         <p><a href="/">ddddd</a></p>
     </div>
@@ -8,11 +10,13 @@
 
 <script>
 import Heading1 from '@/components/atoms/atoms.heading1.vue';
+import FirstView from '@/components/atoms/atoms.firstView.vue';
 
 export default {
     name: 'Home',
     components: {
-        Heading1
+        Heading1,
+        FirstView
     },
     created() {
         document.title = 'shushutona-web';
