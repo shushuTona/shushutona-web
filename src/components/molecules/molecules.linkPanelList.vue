0def907a0261cdaabc6a2ce40e66fd9593816d1b
shushuTona <nagase.syunnsuke@gmail.com>
2021-02-19 02:31:27 +0900

    molecules.linkPanelList.vue追加

---
+++ src/components/molecules/molecules.linkPanelList.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="m-linkPanelList">
+        <ul class="linkPanelList__list">
+            <li
+                v-for="item of listArray"
+                :key="item.id"
+                class="linkPanelList__listItem"
+            >
+                <LinkPanel
+                    :linkHref="item.linkHref"
+                    :mainLinkText="item.mainLinkText"
+                    :subLinkText="item.subLinkText"
+                    :isBlank="true"
+                />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import LinkPanel from '@/components/atoms/atoms.linkPanel.vue';
+
+export default {
+    name: 'LinkPanelList',
+    components: {
+        LinkPanel
+    },
+    props: {
+        listArray: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.m-linkPanelList {
+    margin: 24px 0;
+
+    .linkPanelList {
+        &__listItem {
+            & + .linkPanelList__listItem {
+                margin-top: 8px;
+            }
+        }
+    }
+}
+</style>>
